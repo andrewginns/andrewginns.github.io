119c35feb88fec90eb78307925d89b700c8ede87
Andrew Ginns <ginns.aw@gmail.com>
2025-06-29 20:11:16 +0000

    chore: Address minor problems with ui elements and text formatting

--- tests/merbench-screenshots.spec.ts
+++ tests/merbench-screenshots.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import type { Page } from '@playwright/test';
 
 async function waitForChartsToLoad(page: Page): Promise<void> {
